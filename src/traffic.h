c8b07a0d4a56b0f8c14669570dd82a432e5f7256
Pisit Aussawaniratsai <87642388+TAGCH@users.noreply.github.com>
2023-02-06 14:24:47 +0700

    Update traffic.h

--- src/traffic.h
+++ src/traffic.h
@@ -3,7 +3,7 @@
 #include <ArduinoJson.h>
 #include <Bounce2.h>
 
-const String baseUrl = "https://exceed-hardware-stamp465.koyeb.app";
+const String baseUrl = "https://exceed-hardware-stamp465.koyeb.app/all_traffic";
 
 const String point = "15";
 const int nearby_1 = 14;
@@ -13,7 +13,7 @@ void GET_traffic()
 {
   DynamicJsonDocument doc(65536);
   HTTPClient http;
-  const String url = baseUrl + "/all_traffic";
+  const String url = baseUrl + "/t4u2bf";
   http.begin(url);
 
   Serial.println("Nearby traffic");
@@ -33,6 +33,44 @@ void GET_traffic()
     Serial.println((const char*)doc["all_traffic"][nearby_2]);
 
   }
+  else
+  {
+    Serial.print("Error ");
+    Serial.println(httpResponseCode);
+  }
+
+  Serial.println("----------------------------------");
+}
+
+void POST_traffic(String led)
+{
+  const String url = baseUrl + "/my_traffic?point=" + point;
+  String json;
+  HTTPClient http;
+  http.begin(url);
+  http.addHeader("Content-Type", "application/json");
+
+  DynamicJsonDocument doc(2048);
+  doc["all_traffic"]["point"] = "15";
+  doc["all_traffic"]["traffic"] = led;
+
+  serializeJson(doc, json);
+
+  Serial.println("POST " + led);
+  int httpResponseCode = http.POST(json);
+  if (httpResponseCode == 200)
+  {
+    Serial.print("Done");
+    Serial.println();
+  }
+  else
+  {
+    Serial.print("Error ");
+    Serial.println(httpResponseCode);
+  }
+
+  Serial.println("----------------------------------");
+}
   else
   {
     Serial.print("Error ");
