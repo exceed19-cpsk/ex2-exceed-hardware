1655f0ede26f538404e671db399a2fd24a369a6f
belleqss <bellbi123@gmail.com>
2023-02-06 13:12:44 +0700

    add main

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,13 @@
 #include <Bounce2.h>
 #include "traffic.h"
 
-#define red <led red pin>
-#define yellow <led yellow pin>
-#define green <led green pin>
-#define ldr <ldr pin>
-#define button <button pin>
+#define red 26
+#define yellow 25
+#define green 33
+#define ldr 34
+#define button 27
 
-#define light <แสดงมันมืด มีค่าเท่าไหร่>
+#define light 0
 
 int state = 1;
 int count = 0;
@@ -39,26 +39,51 @@ void setup()
 
 void loop()
 {
+  debouncer.update();
+  int ch = analogRead(ldr);
   // *** write your code here ***
   // Your can change everything that you want
   if (state == 1)
   {
+    digitalWrite(green, HIGH);
+    digitalWrite(red, LOW);
+    if(debouncer.fell()){
+      state = 2;
+    }
+    POST_traffic("green");
+    GET_traffic();
     // while led GREEN
+
   }
   else if (state == 2)
   {
+    digitalWrite(yellow, HIGH);
+    digitalWrite(green, LOW);
+    delay(8000);
+    state = 3;
+    POST_traffic("yellow");
+    GET_traffic();
     // while led YELLOW
   }
   else if (state == 3)
   {
+    digitalWrite(red, HIGH);
+    digitalWrite(yellow, LOW);
+    delay(5000);
+    Serial.println(ch);
+    if(ch < 4095){
+      state = 1;
+    }
+    POST_traffic("red");
     // while led RED
   }
 }
 
+
 void Connect_Wifi()
 {
-  const char *ssid = "Your Wifi Name";
-  const char *password = "Your Wifi Password";
+  const char *ssid = "Galaxy A30C9C8";
+  const char *password = "himl9080";
   WiFi.begin(ssid, password);
   Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED)
